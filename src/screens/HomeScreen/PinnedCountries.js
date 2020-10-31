828ddb73fcd741d3c0bcf52bc1432da27485df00
Fran Grgic <grgicfran@gmail.com>
2020-10-31 12:47:28 +0100

    Implement rounding numbers to two decimals, copy-paste inside text fiels, decimal input

--- src/screens/HomeScreen/PinnedCountries.js
+++ src/screens/HomeScreen/PinnedCountries.js
@@ -15,7 +15,7 @@ const PinnedCountries = () => {
   const width = useWindowDimensions().width;
   const parentWidth = width;
   const childrenWidth = width - 20;
-  const childrenHeight = 60;
+  const childrenHeight = useWindowDimensions().height * 0.15;
 
   const { countries, swapCountries } = useContext(CountriesContext);
   const { rates } = useContext(RatesContext);
@@ -35,14 +35,32 @@ const PinnedCountries = () => {
     const changeIndex = newValues
       .map((e) => Object.keys(e)[0])
       .indexOf(currencyCode);
-    newValues[changeIndex] = { [currencyCode]: value };
+
+    value = value.replace(',', '.');
+    if (value.endsWith('.')) {
+      newValues[changeIndex] = {
+        [currencyCode]: value,
+      };
+      setCurrentValues(newValues);
+      return;
+    }
+
     const valueInEuros = value / rates[currencyCode];
 
-    newValues = newValues.map((newValue) => {
+    newValues = newValues.map((newValue, index) => {
+      if (index === changeIndex) return;
       const currencyRate = rates[Object.keys(newValue)[0]];
       const num = valueInEuros * currencyRate;
-      return { [Object.keys(newValue)[0]]: valueInEuros * currencyRate };
+      return {
+        [Object.keys(newValue)[0]]: parseFloat(value)
+          ? (valueInEuros * currencyRate).toFixed(2)
+          : 0,
+      };
     });
+
+    newValues[changeIndex] = {
+      [currencyCode]: parseFloat(value) ? parseFloat(value) : 0,
+    };
 
     // console.warn(newValues);
 
@@ -54,7 +72,7 @@ const PinnedCountries = () => {
       <View
         style={[styles.item, { width: parentWidth, minHeight: childrenHeight }]}
       >
-        <View>
+        <View style={styles.countryInfoContainer}>
           <PoppinsText italic primary fontSize={ds.fontSize[0]}>
             {item.name}
           </PoppinsText>
@@ -63,17 +81,25 @@ const PinnedCountries = () => {
             {item.currency}
           </PoppinsText>
         </View>
-        <TextInput
-          style={styles.currencyInput}
-          carretHidden
-          blurOnSubmit
-          keyboardType="numeric"
-          onChangeText={(input) => {
-            updateCurrencies(item.currency, input);
-          }}
-          placeholder={JSON.stringify(currentValues[index][item.currency])}
-          value={JSON.stringify(currentValues[index][item.currency])}
-        ></TextInput>
+        <View style={styles.currencyInputContainer}>
+          <TextInput
+            style={styles.currencyInput}
+            carretHidden
+            selectTextOnFocus
+            maxLength={7}
+            blurOnSubmit
+            keyboardType="numeric"
+            onChangeText={(input) => {
+              updateCurrencies(item.currency, input);
+            }}
+            placeholder={JSON.stringify(currentValues[index][item.currency])}
+            value={
+              currentValues[index][item.currency] > 0
+                ? '' + currentValues[index][item.currency]
+                : ''
+            }
+          ></TextInput>
+        </View>
       </View>
     );
   };
@@ -95,7 +121,7 @@ const PinnedCountries = () => {
         parentWidth={parentWidth}
         childrenWidth={childrenWidth}
         childrenHeight={childrenHeight}
-        marginChildrenBottom={30}
+        marginChildrenBottom={0}
         delayLongPress={75}
         onDragEnd={(a, b) => swapCountries(a, b)}
         onDataChange={(newData) => {
@@ -126,13 +152,26 @@ const styles = StyleSheet.create({
     padding: ds.padding[3],
     backgroundColor: ds.dirtyWhite,
   },
-  currencyInput: {
-    borderBottomWidth: 1,
+  countryInfoContainer: {
+    flexGrow: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    maxWidth: '40%',
+  },
+  currencyInputContainer: {
     flexGrow: 1,
     maxWidth: '40%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  currencyInput: {
+    flexGrow: 1,
+    borderBottomWidth: 1,
     padding: ds.padding[3],
     fontSize: ds.fontSize[3],
-    fontFamily: 'poppins-bold',
+    fontFamily: 'poppins',
   },
 });
 
